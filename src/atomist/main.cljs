85c5852575ff6c75b764bd35588e53c88208ba64
James Carnegie <james@atomist.com>
2020-05-04 18:06:15 +0100

    Make it work

--- src/atomist/main.cljs
+++ src/atomist/main.cljs
@@ -2,21 +2,10 @@
   (:require [cljs.pprint :refer [pprint]]
             [cljs.core.async :refer [<! >! timeout chan]]
             [goog.string.format]
+            [atomist.container :as container]
             [atomist.cljs-log :as log]
             [atomist.api :as api])
   (:require-macros [cljs.core.async.macros :refer [go]]))
-
-(defn handle-push [request]
-  (cond
-    (contains? (:data request) :Push)
-    ((-> (api/finished :message "----> Push event handler finished")
-
-         (api/clone-ref)
-         (api/create-ref-from-push-event)
-         (api/skip-push-if-atomist-edited)
-         (api/status)) request)
-    :else
-    (log/warn "did not understand")))
 
 (defn ^:export handler
   "handler
@@ -24,8 +13,11 @@
     params
       data - Incoming Request #js object
       sendreponse - callback ([obj]) puts an outgoing message on the response topic"
-  [data sendreponse]
-  (api/make-request
-   data
-   sendreponse
-   handle-push))
+  []
+  ((-> (api/finished :message "----> Push event handler finished")
+
+       (api/clone-ref)
+       (api/create-ref-from-push-event)
+       (api/skip-push-if-atomist-edited)
+       (api/status)
+       (container/mw-make-container-request)) {}))
