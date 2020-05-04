e23a601cc1b05cb781a910a1421a56ddec795aac
Jim Clark <slimslenderslacks@gmail.com>
2020-05-04 14:42:02 -0700

    just a notes on things I figured out last week

--- src/atomist/main.cljs
+++ src/atomist/main.cljs
@@ -4,20 +4,30 @@
             [goog.string.format]
             [atomist.container :as container]
             [atomist.cljs-log :as log]
-            [atomist.api :as api])
+            [atomist.api :as api]
+            [cljs-node-io.core :as io]
+            [cljs-node-io.proc :as proc])
   (:require-macros [cljs.core.async.macros :refer [go]]))
 
-(defn ^:export handler
-  "handler
-    must return a Promise - we don't do anything with the value
-    params
-      data - Incoming Request #js object
-      sendreponse - callback ([obj]) puts an outgoing message on the response topic"
-  []
-  ((-> (api/finished :message "----> Push event handler finished")
+(defn run-deps-tree [handler]
+  (fn [request]
+    (go
+      (let [atmhome (io/file (.. js/process -env -ATOMIST_HOME))]
+        (if (and (.exists atmhome) (.exists (io/file atmhome "project.clj")))
+          (let [[err stdout stderr] (<! (proc/aexec "lein deps :tree" {:cwd (.getPath atmhome)}))]
+            (log/info stdout)
+            (if err
+              (log/error stderr)))
+          (log/warn "there was no checked out " (.getPath atmhome)))
+        (<! (handler request))))))
 
-       (api/clone-ref)
-       (api/create-ref-from-push-event)
+(defn ^:export handler
+  "no arguments because this handler runs in a container that should fulfill the Atomist container contract
+   the context is extract fro the environment using the container/mw-make-container-request middleware"
+  []
+  ((-> (api/finished :message "----> Push event handler finished"
+                     :success "completed line-deps-tree-skill")
+       (run-deps-tree)
        (api/skip-push-if-atomist-edited)
        (api/status)
        (container/mw-make-container-request)) {}))
