75be7d07da7dffe29ee2f4d0649c70e7c6a8f7c5
James Carnegie <james@atomist.com>
2020-05-06 16:29:53 +0100

    More fields

--- run-local.sh
+++ run-local.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-export WORKSPACE_ID=T29E48P34
-export GRAPHQL_ENDPOINT=https://automation.atomist.com/graphql
+export ATOMIST_WORKSPACE_ID=T29E48P34
+export ATOMIST_GRAPHQL_ENDPOINT=https://automation.atomist.com/graphql
 export ATOMIST_PAYLOAD="$(pwd)/payload.json"
 export ATOMIST_STORAGE=gs://t29e48p34-workspace-storage
 export ATOMIST_CORRELATION_ID=whatever
 export ATOMIST_HOME="$(pwd)/../lore"
+export ATOMIST_TOPIC=projects/atomist-skill-production/topics/packaging-test-topic
+
 export GOOGLE_APPLICATION_CREDENTIALS="$(pwd)/atomist-skill-production-T29E48P34.json"
-export TOPIC=projects/atomist-skill-production/topics/packaging-test-topic
+
 export IN_REPL=true
 node index.js
 
