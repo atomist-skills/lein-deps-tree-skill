af9f2a23d8a90b1dfcdf745ec1ac602ebe00f624
James Carnegie <james@atomist.com>
2020-06-10 13:58:08 +0100

    Doc please

--- src/atomist/main.cljs
+++ src/atomist/main.cljs
@@ -1,123 +1,43 @@
 (ns atomist.main
-  (:require [cljs.pprint :refer [pprint]]
-            [cljs.core.async :refer [<! >! timeout chan]]
-            [goog.string.format]
-            [atomist.container :as container]
+  (:require [atomist.api :as api]
             [atomist.cljs-log :as log]
-            [atomist.api :as api]
+            [atomist.container :as container]
+            [atomist.github :as github]
             [atomist.json :as json]
-            [goog.string :as gstring]
             [cljs-node-io.core :as io]
             [cljs-node-io.proc :as proc]
+            [cljs.core.async :refer [<! >! chan timeout]]
+            [cljs.pprint :refer [pprint]]
             [clojure.string :as str]
-            [cljs-http.client :as client]
-            ["xhr2" :as xhr2]
-            [atomist.graphql-channels :as channels])
+            goog.string.format)
   (:require-macros [cljs.core.async.macros :refer [go]]))
 
-(set! js/XMLHttpRequest xhr2)
+(def check-run {:name "lein deps tree"
+                :status "completed"})
 
-(def issue-title "`lein deps tree` found confusing dependencies")
-(def issue-body-stamp "[atomist:generated:lein-deps-tree-skill]")
-
-(defn list-issues
-  [request & [sha]]
+(defn- failed-run [request stderr]
   (go
-    (try
-      (let [response (<! (client/get "https://api.github.com/search/issues"
-                                     {:query-params {"q" (gstring/format "%s state:open type:issue is:private repo:%s/%s %s in:body"
-                                                                         (or sha "")
-                                                                         (-> request :ref :owner)
-                                                                         (-> request :ref :repo)
-                                                                         issue-body-stamp)}
-                                      :headers {"Authorization" (gstring/format "Bearer %s" (:token request))}}))]
-        (if (= 200 (:status response))
-          (-> response :body)
-          (log/warnf "status %s - %s" (:status response) (-> response :body))))
-      (catch :default ex
-        (log/error "raised exception " ex)))))
+   (let [response (<! (github/create-check request
+                                           (-> request :data :Push first :repo :owner)
+                                           (-> request :data :Push first :repo :name)
+                                           (merge check-run
+                                                  {:conclusion "failure"
+                                                   :head_sha (-> request :data :Push first :after :sha)
+                                                   :output {:title "lein deps :tree failure"
+                                                            :summary stderr}})))]
+     (log/info "check-run status " (:status response)))))
 
-(defn comment-issue
-  [request issue comment]
+(defn- successful-run [request]
   (go
-    (try
-      (let [response (<! (client/post (gstring/format "https://api.github.com/repos/%s/%s/issues/%s/comments"
-                                                      (-> request :ref :owner)
-                                                      (-> request :ref :repo)
-                                                      (:number issue))
-                                      {:body (json/->str {:body comment})
-                                       :headers {"Authorization" (gstring/format "Bearer %s" (:token request))}}))]
-        (if (= 201 (:status response))
-          (-> response :body)
-          (log/warnf "status %s - %s" (:status response) (-> response :body))))
-      (catch :default ex
-        (log/error "raised exception " ex)))))
-
-(defn create-issue
-  [request comment]
-  (go
-    (try
-      (let [response (<! (client/post (gstring/format "https://api.github.com/repos/%s/%s/issues"
-                                                      (-> request :ref :owner)
-                                                      (-> request :ref :repo))
-                                      {:body (json/->str {:title issue-title
-                                                          :body (gstring/format "%s\n\n%s" comment issue-body-stamp)})
-                                       :headers {"Authorization" (gstring/format "Bearer %s" (:token request))}}))]
-        (if (= 201 (:status response))
-          (-> response :body)
-          (log/warnf "status %s - %s" (:status response) (-> response :body))))
-      (catch :default ex
-        (log/error "raised exception " ex)))))
-
-(defn raise-issue-if-not-exists [handler]
-  (fn [request]
-    (go
-      (if (:lein-deps-tree request)
-        (let [sha (-> request :data :Push first :after :sha)]
-          (if (and (:token request))
-            (if-let [issue (-> (<! (list-issues request sha)) :items not-empty first)]
-              (do
-                (log/debugf "Found issue %s for current push, doing nothing" (:number issue))
-                (<! (handler request)))
-              (if-let [issue (some-> (<! (list-issues request)) :items first)]
-                (do
-                  (log/debugf "Found issue %s for another commit, updating" (:number issue))
-                  (<! (comment-issue request issue (gstring/format "Commit %s still has confusing lein dependencies\n\n```%s```" sha (:lein-deps-tree request))))
-                  (<! (handler request)))
-                (do
-                  (log/debugf "Didn't find an issue, creating")
-                  (<! (create-issue request (gstring/format "Commit %s has confusing lein dependencies\n\n```%s```" sha (:lein-deps-tree request))))
-                  (<! (handler request)))))
-            (do
-              (log/warnf "Could not find github token")
-              (<! (api/finish request :failure "Could not find a github token to raise issue")))))
-        (<! (handler request))))))
-
-(defn close-issue
-  [request issue]
-  (go
-    (try
-      (let [response (<! (client/patch (gstring/format "https://api.github.com/repos/%s/%s/issues/%s"
-                                                       (-> request :ref :owner)
-                                                       (-> request :ref :repo)
-                                                       (:number issue))
-                                       {:body (json/->str {:state "closed"})
-                                        :headers {"Authorization" (gstring/format "Bearer %s" (:token request))}}))]
-        (if (= 201 (:status response))
-          (-> response :body)
-          (log/warnf "status %s - %s" (:status response) (-> response :body))))
-      (catch :default ex
-        (log/error "raised exception " ex)))))
-
-(defn close-issue-if-exists
-  [request]
-  (go
-    (log/infof "Closing issue")
-    (doseq [issue (some-> (<! (list-issues request)) :items)]
-      (do
-        (log/infof "Found issue %s for another commit, closing" (:number issue))
-        (-> (comment-issue request issue (gstring/format "Commit %s removed confusing lein dependencies" (-> request :data :Push first :after :sha))))
-        (<! (close-issue request issue))))))
+   (let [response (<! (github/create-check request
+                                           (-> request :data :Push first :repo :owner)
+                                           (-> request :data :Push first :repo :name)
+                                           (merge check-run
+                                                  {:conclusion "success"
+                                                   :head_sha (-> request :data :Push first :after :sha)
+                                                   :output {:title "lein deps :tree success"
+                                                            :summary "No confusing dependencies found"}})))]
+     (log/info "check-run status " (:status response)))))
 
 (defn run-deps-tree [handler]
   (fn [request]
@@ -135,11 +55,12 @@
                 (<! (api/finish request :failure (str "Error running lein deps: " stderr))))
 
               (str/includes? stderr "Possibly confusing dependencies found:")
-              (<! (handler (assoc request :lein-deps-tree (last (str/split stderr #"Possibly\ confusing\ dependencies\ found:")) stderr)))
+              (do (<! (failed-run request (last (str/split stderr #"Possibly\ confusing\ dependencies\ found:"))))
+                  (<! (handler request)))
 
               :else
               (do
-                (<! (close-issue-if-exists request))
+                (<! (successful-run request))
                 (<! (handler request)))))
           (do
             (log/warn "there was no checked out " (.getPath atmhome))
@@ -197,27 +118,6 @@
             (log/info "Found Maven repo credentials, making them available to lein")
             (<! (handler (assoc request :maven {:username (-> rp :credential :owner :login) :password (-> rp :credential :secret)})))))))))
 
-(defn skip-if-not-default-branch
-  "Skip if we only operate on default branches, and this push isn't for it"
-  [handler]
-  (fn [request]
-    (go
-      (let [defaultOnly (some->> request
-                                 :skill
-                                 :configuration
-                                 :instances
-                                 first
-                                 :parameters
-                                 (filter #(= "defaultBranchOnly" (:name %)))
-                                 first
-                                 :value)
-            branch (->> request :data :Push first :branch)
-            defaultBranch (->> request :data :Push first :repo :defaultBranch)]
-        (log/infof "defaultBranchOnly %s, branch: %s, default branch: %s" defaultOnly branch defaultBranch)
-        (if (and defaultOnly (not= (branch defaultBranch)))
-          (<! (api/finish request :success "Doing nothing due to branch configuration"))
-          (<! (handler request)))))))
-
 (defn read-atomist-payload [handler]
   (letfn [(payload->owner [{:keys [data]}]
             (or (-> data :Push first :repo :owner)
@@ -257,13 +157,11 @@
   []
   ((-> (api/finished :message "----> Push event handler finished"
                      :success "completed line-deps-tree-skill")
-       (raise-issue-if-not-exists)
        (run-deps-tree)
        (extract-maven-repo-creds)
        (api/extract-github-token)
        (api/create-ref-from-push-event)
        (api/skip-push-if-atomist-edited)
-       (skip-if-not-default-branch)
        (api/status)
        ;; make sure we don't log any secrets etc...
        ((api/compose-middleware
