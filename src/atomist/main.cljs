239e50a9cad6e7a8c25bcdcc676bcfea61b2cf67
Jim Clark <slimslenderslacks@gmail.com>
2020-06-12 15:19:58 -0700

    * update shadow-cljs to do only simple optimization * add pending check run earlier * leave Dockerfile as is

--- src/atomist/main.cljs
+++ src/atomist/main.cljs
@@ -2,8 +2,6 @@
   (:require [atomist.api :as api]
             [atomist.cljs-log :as log]
             [atomist.container :as container]
-            [atomist.github :as github]
-            [atomist.json :as json]
             [cljs-node-io.core :as io]
             [cljs-node-io.proc :as proc]
             [cljs.core.async :refer [<! >! chan timeout]]
@@ -11,33 +9,6 @@
             [clojure.string :as str]
             goog.string.format)
   (:require-macros [cljs.core.async.macros :refer [go]]))
-
-(def check-run {:name "lein deps tree"
-                :status "completed"})
-
-(defn- failed-run [request stderr]
-  (go
-   (let [response (<! (github/create-check request
-                                           (-> request :data :Push first :repo :owner)
-                                           (-> request :data :Push first :repo :name)
-                                           (merge check-run
-                                                  {:conclusion "failure"
-                                                   :head_sha (-> request :data :Push first :after :sha)
-                                                   :output {:title "lein deps :tree failure"
-                                                            :summary stderr}})))]
-     (log/info "check-run status " (:status response)))))
-
-(defn- successful-run [request]
-  (go
-   (let [response (<! (github/create-check request
-                                           (-> request :data :Push first :repo :owner)
-                                           (-> request :data :Push first :repo :name)
-                                           (merge check-run
-                                                  {:conclusion "success"
-                                                   :head_sha (-> request :data :Push first :after :sha)
-                                                   :output {:title "lein deps :tree success"
-                                                            :summary "No confusing dependencies found"}})))]
-     (log/info "check-run status " (:status response)))))
 
 (defn run-deps-tree [handler]
   (fn [request]
@@ -55,13 +26,16 @@
                 (<! (api/finish request :failure (str "Error running lein deps: " stderr))))
 
               (str/includes? stderr "Possibly confusing dependencies found:")
-              (do (<! (failed-run request (last (str/split stderr #"Possibly\ confusing\ dependencies\ found:"))))
-                  (<! (handler request)))
+              (<! (handler (assoc request
+                             :checkrun/conclusion "failure"
+                             :checkrun/output {:title "lein deps :tree failure"
+                                               :summary stderr})))
 
               :else
-              (do
-                (<! (successful-run request))
-                (<! (handler request)))))
+              (<! (handler (assoc request
+                             :checkrun/conclusion "success"
+                             :checkrun/output {:title "lein deps :tree success"
+                                               :summary "No confusing dependencies found"})))))
           (do
             (log/warn "there was no checked out " (.getPath atmhome))
             (<! (api/finish request :failure "Failed to checkout"))))))))
@@ -118,38 +92,6 @@
             (log/info "Found Maven repo credentials, making them available to lein")
             (<! (handler (assoc request :maven {:username (-> rp :credential :owner :login) :password (-> rp :credential :secret)})))))))))
 
-(defn read-atomist-payload [handler]
-  (letfn [(payload->owner [{:keys [data]}]
-            (or (-> data :Push first :repo :owner)
-                (-> data :Tag first :commit :repo :owner)))
-          (payload->repo [{:keys [data]}]
-            (or (-> data :Push first :repo :name)
-                (-> data :Tag first :commit :repo :name)))]
-    (fn [request]
-      (go
-        (api/trace "read-atomist-payload")
-        (try
-          (let [payload (-> (io/file (:payload request))
-                            (io/slurp)
-                            (json/->obj)
-                            (api/event->request))]
-            (log/info "extensions " (:extensions payload))
-            (log/info "skill " (:skill payload))
-            (log/info "secrets " (map :uri (:secrets payload)))
-           ;;(log/info "data " (:data payload))
-            (if (contains? (:data payload) :Push)
-              (<! (handler
-                   (-> request
-                       (assoc :owner (payload->owner payload) :repo (payload->repo payload))
-                       (merge payload {:api-key (->> payload :secrets (filter #(= "atomist://api-key" (:uri %))) first :value)}))))
-              (do
-                (when-let [f (io/file (:dir request))]
-                  (log/infof "%s - %s" (.getPath f) (.exists f))
-                  (log/infof "Tag %s - %s" (-> request :data :Tag :name) (-> request :data :Tag :description)))
-                (<! (api/finish request :success "not building new Tag event")))))
-          (catch :default ex
-            (log/error ex)
-            request))))))
 
 (defn ^:export handler
   "no arguments because this handler runs in a container that should fulfill the Atomist container contract
@@ -158,15 +100,11 @@
   ((-> (api/finished :message "----> Push event handler finished"
                      :success "completed line-deps-tree-skill")
        (run-deps-tree)
+       (api/with-github-check-run :name "lein deps tree")
        (extract-maven-repo-creds)
        (api/extract-github-token)
-       (api/create-ref-from-push-event)
+       (api/create-ref-from-event)
        (api/skip-push-if-atomist-edited)
        (api/status)
-       ;; make sure we don't log any secrets etc...
-       ((api/compose-middleware
-         [read-atomist-payload]
-         [container/create-logger]
-         [container/check-environment]
-         [container/make-container-request])))
+       (container/make-container-request))
    {}))
