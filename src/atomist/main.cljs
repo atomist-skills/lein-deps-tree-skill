5bb530108c0005c851c0164c3b3743d722aef496
James Carnegie <james@atomist.com>
2020-09-28 09:47:51 +0100

    Use env vars instead of ad-hoc graphql query

--- src/atomist/main.cljs
+++ src/atomist/main.cljs
@@ -23,6 +23,11 @@
             [clojure.string :as str])
   (:require-macros [cljs.core.async.macros :refer [go]]))
 
+(defn -js->clj+
+  "For cases when built-in js->clj doesn't work. Source: https://stackoverflow.com/a/32583549/4839573"
+  [x]
+  (into {} (for [k (js-keys x)] [k (aget x k)])))
+
 (defn run-deps-tree [handler]
   (fn [request]
     (go
@@ -30,12 +35,14 @@
             atmhome (io/file ATM-HOME)]
         (if (and (.exists atmhome) (.exists (io/file atmhome "project.clj")))
           (let [[err stdout stderr] (<! (proc/aexec "lein deps :tree-data" {:cwd (.getPath atmhome)
-                                                                            :env {"ARTIFACTORY_USER" (-> request :maven :username)
-                                                                                  "MVN_ARTIFACTORYMAVENREPOSITORY_USER" (-> request :maven :username)
-                                                                                  "ARTIFACTORY_PWD" (-> request :maven :password)
-                                                                                  "MVN_ARTIFACTORYMAVENREPOSITORY_PWD" (-> request :maven :password)
-                                                                                  ;; use atm-home for .m2 directory
-                                                                                  "_JAVA_OPTIONS" (str "-Duser.home=" ATM-HOME)}}))]
+                                                                            :env (-> (-js->clj+ (.. js/process -env))
+                                                                                     (merge
+                                                                                      {"MVN_ARTIFACTORYMAVENREPOSITORY_USER"
+                                                                                       (.. js/process -env -MVN_ARTIFACTORYMAVENREPOSITORY_USER)
+                                                                                       "MVN_ARTIFACTORYMAVENREPOSITORY_PWD"
+                                                                                       (.. js/process -env -MVN_ARTIFACTORYMAVENREPOSITORY_PWD)
+                                                                                       ;; use atm-home for .m2 directory
+                                                                                       "_JAVA_OPTIONS" (str "-Duser.home=" atm-home)}))}))]
             (cond
 
               err
@@ -58,58 +65,6 @@
             (log/warn "there was no checked out " (.getPath atmhome))
             (<! (api/finish request :failure "Failed to checkout"))))))))
 
-(defn extract-maven-repo-creds
-  "TODO - make this work for many repositories"
-  [handler]
-  (fn [request]
-    (go
-      (let [rp-id (some->> request
-                           :skill
-                           :configuration
-                           :instances
-                           first
-                           :resourceProviders
-                           (filter #(= "maven" (:name %)))
-                           first
-                           :selectedResourceProviders
-                           first
-                           :id)
-            rp (some->> request
-                        :data
-                        :Push
-                        first
-                        :repo
-                        :org
-                        :team
-                        :resourceProviders
-                        (filter #(= rp-id (:id %)))
-                        first)]
-        (cond
-          (not rp-id)
-          (do
-            (log/warn "could not find maven resource provider id in skill config payload ")
-            (<! (handler request)))
-
-          (and rp-id (not rp))
-          (do
-            (log/warn "Found Maven repo id in config, but not in event payload - this probably will not work")
-            (<! (handler request)))
-
-          (not (-> rp :credential :secret))
-          (do
-            (log/warn "Found Maven repo id in config and payload, but not secret was found. Let's hope it's not needed")
-            (<! (handler request)))
-
-          (not (-> rp :credential :owner :login))
-          (do
-            (log/warn "Couldn't find maven repo username, let's hope we don't need it")
-            (<! (handler request)))
-
-          :otherwise
-          (do
-            (log/info "Found Maven repo credentials, making them available to lein")
-            (<! (handler (assoc request :maven {:username (-> rp :credential :owner :login) :password (-> rp :credential :secret)})))))))))
-
 (defn cancel-if-not-lein [handler]
   (fn [request]
     (go
@@ -130,7 +85,6 @@
                      :success "completed line-deps-tree-skill")
        (run-deps-tree)
        (api/with-github-check-run :name "lein-deps-tree-skill")
-       (extract-maven-repo-creds)
        (cancel-if-not-lein)
        (api/extract-github-token)
        (api/create-ref-from-event)
